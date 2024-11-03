c89add3486045b1b5cf800b956b4f372071e515e
slince <taosikai@yeah.net>
2024-11-04 00:24:53 +0800

    init

--- examples/cluster.php
+++ examples/cluster.php
@@ -51,17 +51,19 @@ $cluster = Cluster::create(function(Cluster $cluster){
 }, $logger);
 
 if ($cluster->primary) {
-    $worker = $cluster->fork();
-    $worker->on('message', function (string $message) use($cluster){
-        $cluster->logger()->info(sprintf('received message from worker: %s', $message));
-    });
-    $worker->on('ping', function() use($worker, $cluster){
-        $cluster->logger()->info(sprintf('the worker %d is alive', $worker->getId()));
-    });
-    $worker->on('close', function () use($cluster){
+    for ($i = 0; $i < 3; $i++) {
+        $worker = $cluster->fork();
+        $worker->on('message', function (string $message) use ($cluster) {
+            $cluster->logger()->info(sprintf('received message from worker: %s', $message));
+        });
+        $worker->on('ping', function () use ($worker, $cluster) {
+            $cluster->logger()->info(sprintf('the worker %d is alive', $worker->getId()));
+        });
+        $worker->on('close', function () use ($cluster) {
 //        $cluster->logger()->info('fork new worker');
 //        $cluster->fork();
-    });
+        });
+    }
     $cluster->on('work.close', function(Worker $worker) use($cluster){
         $cluster->logger()->warning(sprintf('the worker %d is closed', $worker->getId()));
         echo $worker->getOutput();
